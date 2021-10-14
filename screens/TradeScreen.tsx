db6528e5fd8244d590c258bbd7ebb2ec4d11da8f
Travisse Hansen <travisseh@gmail.com>
2021-10-13 20:52:58 -0600

    got page mostly laid out on TradeScreen

--- screens/TradeScreen.tsx
+++ screens/TradeScreen.tsx
@@ -45,12 +45,96 @@ const TradeScreen = ({ navigation, route }: Props) => {
 			>
 				Trade {token.name}{' '}
 			</SubPageHeader>
+			<View>
+				<Text style={{ ...styles.bigNumber, alignSelf: 'center' }}>
+					$0
+				</Text>
+			</View>
+			<View>
+				<View
+					style={{
+						flexDirection: 'row',
+						alignSelf: 'stretch',
+						justifyContent: 'space-between',
+						marginHorizontal: 16,
+						marginBottom: 16,
+					}}
+				>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>1</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>2</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>3</Text>
+					</View>
+				</View>
+				<View
+					style={{
+						flexDirection: 'row',
+						alignSelf: 'stretch',
+						justifyContent: 'space-between',
+						marginHorizontal: 16,
+						marginBottom: 16,
+					}}
+				>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>4</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>5</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>6</Text>
+					</View>
+				</View>
+				<View
+					style={{
+						flexDirection: 'row',
+						alignSelf: 'stretch',
+						justifyContent: 'space-between',
+						marginHorizontal: 16,
+						marginBottom: 16,
+					}}
+				>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>7</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>8</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>9</Text>
+					</View>
+				</View>
+				<View
+					style={{
+						flexDirection: 'row',
+						alignSelf: 'stretch',
+						justifyContent: 'space-between',
+						marginHorizontal: 16,
+					}}
+				>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>.</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>0</Text>
+					</View>
+					<View style={styles.numberContainer}>
+						<Text style={styles.mediumNumber}>3</Text>
+					</View>
+				</View>
+			</View>
+			<View style={{ marginBottom: 40 }}>
+				<Button>Review Trade</Button>
+			</View>
 		</Background>
 	);
 };
 
-{
-	/* const styles = StyleSheet.create({
+const styles = StyleSheet.create({
 	tableLabel: {
 		fontSize: 14,
 		color: '#727D8D',
@@ -59,7 +143,22 @@ const TradeScreen = ({ navigation, route }: Props) => {
 		fontSize: 17,
 		color: theme.colors.primary,
 	},
-}); */
-}
+	bigNumber: {
+		fontSize: 84,
+		fontFamily: 'Nunito Sans',
+		fontWeight: '400',
+		color: theme.colors.black_two,
+	},
+	mediumNumber: {
+		fontSize: 48,
+		fontFamily: 'Nunito Sans',
+		fontWeight: '400',
+		color: theme.colors.black_one,
+	},
+	numberContainer: {
+		width: 56,
+		height: 66,
+	},
+});
 
 export default memo(TradeScreen);
