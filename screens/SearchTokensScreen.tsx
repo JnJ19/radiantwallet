00a8a2d01d168b07a1260a044e9e0663267e42c4
Travisse Hansen <travisseh@gmail.com>
2021-10-26 10:47:40 -0600

    fixed styling of browse search input

--- screens/SearchTokensScreen.tsx
+++ screens/SearchTokensScreen.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useState, useEffect } from 'react';
-import { Text, TouchableOpacity, TextInput as RNTextInput } from 'react-native';
+import { Text, TouchableOpacity, TextInput as TextInputRN } from 'react-native';
 import { Background, TextInput } from '../components';
 import { Navigation } from '../types';
 import { View, FlatList, Image } from 'react-native';
@@ -10,6 +10,7 @@ import TokenCard from '../components/TokenCard';
 import { useStoreState, useStoreActions } from '../hooks/storeHooks';
 
 import { TokenListProvider, TokenInfo } from '@solana/spl-token-registry';
+import Colors from '../constants/Colors';
 
 type Props = {
 	navigation: Navigation;
@@ -102,10 +103,10 @@ const SearchTokensScreen = ({ navigation }: Props) => {
 
 	return (
 		<Background>
-			<View>
+			<View style={{ marginBottom: 24 }}>
 				<SubPageHeader backButton={false}>Browse Tokens</SubPageHeader>
 
-				<TextInput
+				{/* <TextInput
 					label="Search token name or symbol"
 					mode="outlined"
 					value={search}
@@ -118,10 +119,41 @@ const SearchTokensScreen = ({ navigation }: Props) => {
 							},
 						},
 					}}
-				/>
+				/> */}
 				{/* <View>
 					<RNTextInput  />
 				</View> */}
+				<View
+					style={{
+						height: 64,
+						borderWidth: 1,
+						borderColor: theme.colors.black_seven,
+						borderRadius: 18,
+						padding: 16,
+						flexDirection: 'row',
+						alignItems: 'center',
+					}}
+				>
+					<Image
+						source={require('../assets/icons/Search.png')}
+						style={{ width: 24, height: 24, marginRight: 8 }}
+					/>
+
+					<TextInputRN
+						style={{
+							borderColor: 'black',
+							borderWidth: 0,
+							...theme.fonts.Nunito_Sans.Body_M_Regular,
+						}}
+						onChangeText={(text: string) => setSearch(text)}
+						value={search}
+						placeholder="Search token name or symbol"
+						// placeholderTextColor: theme.colors.primary
+						keyboardType="default"
+						// multiline={true}
+						// minHeight={150}
+					/>
+				</View>
 			</View>
 
 			{/* <FlatList
