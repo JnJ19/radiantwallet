2ab7e9f829ae638e8fcb1858fd169fd06623d10e
Travisse Hansen <travisseh@gmail.com>
2021-11-27 13:31:34 -0700

    and another change for eas

--- metro.config.js
+++ metro.config.js
@@ -1,28 +1,73 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-module.exports = {
-	resolver: {
-		sourceExts: [
-			...defaultConfig.resolver.sourceExts,
-			'svg',
-			'jsx',
-			'js',
-			'ts',
-			'tsx',
-			'cjs',
-		],
-		// extraNodeModules: {
-		//   stream: require.resolve("readable-stream"),
-		// },
-		extraNodeModules: require('node-libs-expo'),
-		assetExts: [
-			...defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
-		],
-		transformer: {
-			babelTransformerPath: require.resolve(
-				'react-native-svg-transformer',
-			),
+// const defaultConfig = getDefaultConfig(./);
+// const defaultConfig = getDefaultConfig(__dirname);
+// module.exports = {
+// 	resolver: {
+// 		sourceExts: [
+// 			...defaultConfig.resolver.sourceExts,
+// 			'svg',
+// 			'jsx',
+// 			'js',
+// 			'ts',
+// 			'tsx',
+// 			'cjs',
+// 		],
+// 		// extraNodeModules: {
+// 		//   stream: require.resolve("readable-stream"),
+// 		// },
+// 		extraNodeModules: require('node-libs-expo'),
+// 		assetExts: [
+// 			...defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
+// 		],
+// 		transformer: {
+// 			babelTransformerPath: require.resolve(
+// 				'react-native-svg-transformer',
+// 			),
+// 		},
+// 	},
+// };
+module.exports = async () => {
+	const {
+		resolver: { sourceExts, assetExts },
+	} = await getDefaultConfig(__dirname);
+
+	return {
+		resolver: {
+			sourceExts: [...sourceExts, 'svg', 'jsx', 'js', 'ts', 'tsx', 'cjs'],
+			extraNodeModules: require('node-libs-expo'),
+			assetExts: [...assetExts.filter((ext) => ext !== 'svg')],
+			transformer: {
+				babelTransformerPath: require.resolve(
+					'react-native-svg-transformer',
+				),
+			},
 		},
-	},
+	};
 };
+
+// {
+// 	resolver: {
+// 		sourceExts: [
+// 			...defaultConfig.resolver.sourceExts,
+// 			'svg',
+// 			'jsx',
+// 			'js',
+// 			'ts',
+// 			'tsx',
+// 			'cjs',
+// 		],
+// 		// extraNodeModules: {
+// 		//   stream: require.resolve("readable-stream"),
+// 		// },
+// 		extraNodeModules: require('node-libs-expo'),
+// 		assetExts: [
+// 			...defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
+// 		],
+// 		transformer: {
+// 			babelTransformerPath: require.resolve(
+// 				'react-native-svg-transformer',
+// 			),
+// 		},
+// 	},
+// };
