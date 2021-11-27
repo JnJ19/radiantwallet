82c99ba75a68d31cf7e3dec432102ccc38c350ed
Travisse Hansen <travisseh@gmail.com>
2021-11-27 12:09:10 -0700

    some stuff to try and make eas build work

--- metro.config.js
+++ metro.config.js
@@ -1,28 +1,24 @@
-const { getDefaultConfig } = require('@expo/metro-config');
+import { getDefaultConfig } from '@expo/metro-config';
 
 const defaultConfig = getDefaultConfig(__dirname);
-module.exports = {
-	resolver: {
-		sourceExts: [
-			...defaultConfig.resolver.sourceExts,
-			'svg',
-			'jsx',
-			'js',
-			'ts',
-			'tsx',
-			'cjs',
-		],
-		// extraNodeModules: {
-		//   stream: require.resolve("readable-stream"),
-		// },
-		extraNodeModules: require('node-libs-expo'),
-		assetExts: [
-			...defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
-		],
-		transformer: {
-			babelTransformerPath: require.resolve(
-				'react-native-svg-transformer',
-			),
-		},
+export const resolver = {
+	sourceExts: [
+		...defaultConfig.resolver.sourceExts,
+		'svg',
+		'jsx',
+		'js',
+		'ts',
+		'tsx',
+		'cjs',
+	],
+	// extraNodeModules: {
+	//   stream: require.resolve("readable-stream"),
+	// },
+	extraNodeModules: require('node-libs-expo'),
+	assetExts: [
+		...defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
+	],
+	transformer: {
+		babelTransformerPath: require.resolve('react-native-svg-transformer'),
 	},
 };
